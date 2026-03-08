0e48b7b46f0fd60ad7d8331f31490a4006a1a5e8
Leela Bhavani <leelabhavani265@gmail.com>
2026-03-08 15:20:23 +0530

    Added backend api files

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("epress");
+const cors = require("cors");
+require("dotenv").config();
+
+const logger = require("./middleware/logger");
+const courseRoutes = require("./routes/courses")
+
+const app=express();
+
+app.use(cors());
+app.use(express.json());
+app.use(logger);
+
+app.use("/api",courseRoutes);
+
+app.get("/",(req,res) => {
+    res.send("Course API is running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+    console.log(Server running on port ${PORT});
+});
